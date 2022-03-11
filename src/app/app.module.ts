e60245da124a649e29f64910ececd6bdf50be729
kimyoungjin98 <gyu250@naver.com>
2022-03-11 14:46:15 +0900

    skills page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { IonicModule } from '@ionic/angular';
 import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProjectComponent } from './project/project.component';
 import { SkillsComponent } from './skills/skills.component';
-import { HomeComponent } from './home/home.component';
-
+import { SkillsModule } from './skills/skills.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		SkillsComponent,
 		ProjectComponent,
 		ContactComponent,
 		AboutComponent,
-  HomeComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		IonicModule.forRoot(),
+		SkillsModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
---
+++ src/app/skills/skills.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
+import { SkillsComponent } from "./skills.component";
+
+@NgModule({
+	declarations: [SkillsComponent],
+	imports: [IonicModule],
+	exports: [SkillsComponent],
+})
+
+export class SkillsModule { }
